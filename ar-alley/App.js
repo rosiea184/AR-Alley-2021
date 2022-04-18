38ec69ccb58b34706e4a84aca1e2a835d485c9a7
Alexandria Wizes <rosiea184@yahoo.co.uk>
2022-04-18 16:36:51 -0400

    created basis for qr scanning

--- ar-alley/App.js
+++ ar-alley/App.js
@@ -1,11 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState('Not yet scanned')
+
+  const askForCameraPermission = () => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status == 'granted')
+    })()
+  }
+
+//Request Camera Permission
+useEffect(() => {
+  askForCameraPermission();
+}, []);
+
+//What happens when we scan bar code
+const handleBarCodeScanned = ({type, data}) => {
+  setScanned(true);
+  setText(data);
+  console.log('Type: ' + type + '\nData: ' + data)
+}
+
+//Check Permissions and return the screens
+if (hasPermission === null){
+  <View style={styles.container}>
+    <Text>Requesting for camera permission</Text>
+  </View>
+}
+if (hasPermission === false){
+  return(
+    <View style={styles.container}>
+      <Text style={{margin: 10}}>No access to camera</Text>
+      <button title={'Allow Camers'} onPress={() => askForCameraPermission()}/>
+    </View>    
+  )
+}
+// Return the View
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.barcodebox}>
+        <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={{height: 400, width: 400}} />
+    </View>
+    <Text style={styles.maintext}>{data}</Text>
+    {scanned && <Button title={'Scan again?'} onPress={() => setScanned(false)} color='tomato'/>}
     </View>
   );
 }
@@ -17,4 +63,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  barcodebox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 300,
+    width: 300,
+    overflow: 'hidden',
+    borderRadius: 30,
+    backgroundColor: 'tomato'
+  }
 });
